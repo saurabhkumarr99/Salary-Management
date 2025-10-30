1b6b12cba866d7c50a51f3115764a7b920bd1b01
saurabhkumarr99 <saurabhkumarr99@gmail.com>
2025-10-30 18:54:09 +0530

    refactor: add structured logging in service and exception handler

--- src/main/java/com/incubyte/SalaryManagement/exceptions/EmployeeGlobalExceptionHandler.java
+++ src/main/java/com/incubyte/SalaryManagement/exceptions/EmployeeGlobalExceptionHandler.java
@@ -4,25 +4,30 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
-
 import com.incubyte.SalaryManagement.dto.ErrorResponseDto;
 
 @RestControllerAdvice
 public class EmployeeGlobalExceptionHandler {
 
+	private static final Logger logger = LoggerFactory.getLogger(EmployeeGlobalExceptionHandler.class);
+
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<ErrorResponseDto> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
-		List<String> errors = ex.getBindingResult().getFieldErrors().stream().map(error -> error.getField() + ": " + error.getDefaultMessage())
-				.collect(Collectors.toList());
+		List<String> errors = ex.getBindingResult().getFieldErrors().stream()
+				.map(error -> error.getField() + ": " + error.getDefaultMessage()).collect(Collectors.toList());
 
 		ErrorResponseDto errorResponseDto = new ErrorResponseDto(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(),
 				"Validation failed", errors);
+
+		logger.error("Validation failed: {}", errors);
 
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponseDto);
 	}
--- src/main/java/com/incubyte/SalaryManagement/service/EmployeeService.java
+++ src/main/java/com/incubyte/SalaryManagement/service/EmployeeService.java
@@ -1,5 +1,7 @@
 package com.incubyte.SalaryManagement.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,8 +15,12 @@ public class EmployeeService {
 	@Autowired
 	EmployeeRepository employeeRepository;
 
+	 private static final Logger logger = LoggerFactory.getLogger(EmployeeService.class);
+	 
 	// Create Employee
 	public Employee createEmployee(@RequestBody Employee employee) {
+		
+		logger.info("Employee created successfully : "+employee);
 		return employeeRepository.save(employee);
 	}
 }
