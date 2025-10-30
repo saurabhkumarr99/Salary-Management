bfe40f996cdd458e7d2a931399bbe1da4e50ee35
saurabhkumarr99 <saurabhkumarr99@gmail.com>
2025-10-30 16:41:00 +0530

    test: add failing test for creating employee (RED)

---
+++ src/test/java/com/incubyte/SalaryManagement/controller/EmployeeControllerTest.java
@@ -0,0 +1,34 @@
+package com.incubyte.SalaryManagement.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmployeeControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper; // Converts object → JSON
+
+	// Test Create Employee
+	@Test
+	void shouldCreateEmployeeSuccessfully() throws Exception {
+		Employee employee = new Employee("John Doe", "Engineer", "India", 80000);
+
+		mockMvc.perform(post("/api/createEmployee").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(employee))).andExpect(status().isCreated())
+				.andExpect(jsonPath("$.id").exists()).andExpect(jsonPath("$.fullName").value("John Doe"))
+				.andExpect(jsonPath("$.salary").value(80000));
+	}
+}
