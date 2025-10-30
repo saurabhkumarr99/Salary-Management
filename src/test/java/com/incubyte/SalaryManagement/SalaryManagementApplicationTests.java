9aa14bc31ccf67c76daaa12e2c1490fd72c0a961
saurabhkumarr99 <saurabhkumarr99@gmail.com>
2025-10-30 16:37:59 +0530

    Initiated project, added dependecies, create project setup

---
+++ src/test/java/com/incubyte/SalaryManagement/SalaryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incubyte.SalaryManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
