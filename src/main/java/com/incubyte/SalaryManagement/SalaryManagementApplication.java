5ae11766b12e0c6fe25877f7220c7d1ed3dae82f
saurabhkumarr99 <saurabhkumarr99@gmail.com>
2025-11-16 17:34:23 +0530

    Added Admin UI

--- src/main/java/com/incubyte/SalaryManagement/SalaryManagementApplication.java
+++ src/main/java/com/incubyte/SalaryManagement/SalaryManagementApplication.java
@@ -3,7 +3,10 @@ package com.incubyte.SalaryManagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 @SpringBootApplication
+@EnableAdminServer
 public class SalaryManagementApplication {
 
 	public static void main(String[] args) {
